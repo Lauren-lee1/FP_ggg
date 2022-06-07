93d7726df1f7dc65314fe5b654762e4966f9d14e
KevinLi0711 <kli30@stuy.edu>
2022-06-07 01:12:18 -0400

    formal end of game + capture(_

--- v6/Game.java
+++ v6/Game.java
@@ -1,3 +1,4 @@
+package v6;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
---
+++ v6/Woo.java
@@ -0,0 +1,10 @@
+package v6;
+public class Woo{
+  /*
+  Create a queue?stack?list? of men that the player can play through
+  */
+  public static void main(String[] arg){
+      Game game = new Game();
+  }
+
+}
