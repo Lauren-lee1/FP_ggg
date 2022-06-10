db2be2751e2d33703ffd1da5d8adac63dae52c16
Lauren Lee <llee30@stuy.edu>
2022-06-10 00:00:06 -0400

    set lee story

--- LLee.java
+++ LLee.java
@@ -10,6 +10,11 @@ public class LLee extends LoveInterest{
     public void setStory() {
         //root
         _story = new StoryNode(_source + "intro.txt");
+        StoryNode tennis = new StoryNode(_source + "tennis/tennis.txt");
+        _story.setleft(tennis);
+
+        StoryNode rock = new StoryNode(_source + "rock/rock.txt");
+        _story.setMind(rock);
     }
 
     public void receiveItem() {
